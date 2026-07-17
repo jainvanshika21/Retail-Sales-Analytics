00a1b18686a77b0753b28e3bcee903f768abac0f
Vanshika Jain <jainvanshika152020@gmail.com>
2026-07-17 18:29:02 +0530

    Fixed Streamlit cloud CSV path

--- streamlit/app.py
+++ streamlit/app.py
@@ -64,10 +64,10 @@ h1,h2,h3{
 def load_data():
 
     try:
-        db_user = os.getenv("DB_USER", "root")
+        db_user = os.getenv("DB_USER")
         db_password = os.getenv("DB_PASSWORD")
-        db_host = os.getenv("DB_HOST", "localhost")
-        db_name = os.getenv("DB_NAME", "sales_project")
+        db_host = os.getenv("DB_HOST")
+        db_name = os.getenv("DB_NAME")
 
         engine = create_engine(f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}"
     )
